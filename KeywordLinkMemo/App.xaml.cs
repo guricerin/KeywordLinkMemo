b0b14aeaf9d97600855658441443e33d16b24025
guricerin <chanbo1e9@gmail.com>
2022-02-07 11:42:56 +0900

    initial commit

---
+++ KeywordLinkMemo/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using KeywordLinkMemo.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace KeywordLinkMemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
